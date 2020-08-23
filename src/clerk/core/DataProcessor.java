39b450a28608aac2874e8972e45a8ef91ec575ee
timurbey <atpoverload@gmail.com>
2020-08-23 15:34:47 -0600

    small rename

---
+++ src/clerk/core/DataProcessor.java
@@ -0,0 +1,10 @@
+package clerk.core;
+
+/** Interface that consumes and produces data. This is a strictly typed interface. */
+public interface DataProcessor<I, O> {
+  /** Adds data to the processor's storage. */
+  default void add(I i);
+
+  /** Returns the result of the processor on stored data. */
+  O process();
+}
