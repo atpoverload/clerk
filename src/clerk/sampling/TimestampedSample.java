d3207a9a067bc40898b8db067df77ad4216390b7
timurbey <atpoverload@gmail.com>
2020-08-24 21:18:26 -0600

    removed the unneeded sample logic

--- src/clerk/sampling/TimestampedSample.java
+++
@@ -1,9 +0,0 @@
-package clerk.sampling;
-
-import clerk.core.Sample;
-import java.time.Instant;
-
-/** Interface for a timestamped sample. */
-public interface TimestampedSample extends Sample {
-  Instant getTimestamp();
-}
