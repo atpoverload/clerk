82e8ac0c39add7f519caaa1f884912ccaa933016
timurbey <atpoverload@gmail.com>
2020-11-04 21:07:42 -0700

    pushing some clean up and test stuff

---
+++ clerk/testing/inject/TestExecutorModule.java
@@ -0,0 +1,31 @@
+package clerk.inject;
+
+import dagger.Module;
+import dagger.Provides;
+import dagger.multibindings.IntoMap;
+import dagger.multibindings.StringKey;
+import java.time.Duration;
+import java.util.concurrent.ScheduledExecutorService;
+
+/** Module to provide test functionality. */
+@Module
+public interface TestExecutorModule {
+  @Provides
+  @IntoMap
+  @StringKey("default_period")
+  @ClerkComponent
+  static Duration provideDefaultPeriod() {
+    return Duration.ofMillis(0);
+  }
+
+  @Provides
+  @ClerkComponent
+  static ScheduledExecutorService provideExecutor() {
+    return newSingleThreadScheduledExecutor(
+        r -> {
+          Thread t = new Thread(r);
+          t.setDaemon(true);
+          return t;
+        });
+  }
+}
